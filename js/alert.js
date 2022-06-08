77bb6459c44ec2179bcc4f46f0ddaf9ef9fb3617
joanmsolis <j.m.s.l.19@hotmail.com>
2022-06-08 06:38:16 -0600

    introducion a la consola y a las variable

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,27 @@
-alert("hola mundo de javascript");
+//console.log("hola mundo de javascript");
+
+// let a = 'imprimir en consola';
+//     b = "esto es para prevenir";
+//     c = "esto es un error";
+//     d = 25;
+//     e = 30;
+//     f = d + e;
+
+// console.table({a,b,c});
+// console.log(f);
+
+let edad =[18,35,46,60]
+
+edad.forEach(function (elemento,indice,array) {
+    //console.log(elemento);
+ 
+    if(elemento <= 18){
+        console.log(elemento +" eres menor de edad");
+    }else if(elemento <=35){
+     console.log(elemento+" eren joven");
+    }else if(elemento <= 46){
+        console.log(elemento +" eres un adulto joven");
+    }else {
+        console.log(elemento+" ya esta viejo");
+    }
+})
---
+++ js/app.js
@@ -0,0 +1,9 @@
+let a = 10;
+var b = 15;
+const c =20;
+let x = a + b;
+let z = 'esto fue un error';
+console.log(a,b,c);
+console.warn(x);
+console.error(z);
+console.table({a, b, c, x, z});
